3b5ea1c1ebaeb35adc62f1fe9d9871a2882e7818
aprack <andrew.prack@gmail.com>
2025-01-08 14:40:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 df = pd.read_csv(r"https://raw.githubusercontent.com/aprack/DGApp/refs/heads/main/dgdiscs.csv")
 
 st.title("Disc Flight Number Search")
-column = st.selectbox("Choose a column to filter:",df.loc[:,['Speed', 'Glide', 'Turn', 'Fade']]
+column = st.selectbox("Choose a column to filter:",df.loc[:,['Speed', 'Glide', 'Turn', 'Fade']])
 value = st.number_input(f"Enter the value to filter in {column}:")
 
 if st.button("Search"):
