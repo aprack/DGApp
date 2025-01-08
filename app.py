610a9ddd4c1139d9814fdc3dbfa5f7f8ead408f8
aprack <andrew.prack@gmail.com>
2025-01-08 14:17:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 # Load the CSV file
-df = pd.read_csv(r"https://raw.githubusercontent.com/aprack/DGApp/refs/heads/main/pdga-approved-disc-golf-discs_2025-01-08T15-27-01.csv")
+df = pd.read_csv(r"https://raw.githubusercontent.com/aprack/DGApp/refs/heads/main/dgdiscs.csv")
 
 st.title("CSV Query App")
 column = st.selectbox("Choose a column to filter:", df.columns)
