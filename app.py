48141377239e674d82acfc4e85d0e90be8d02c1d
aprack <andrew.prack@gmail.com>
2025-01-08 14:33:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # Load the CSV file
 df = pd.read_csv(r"https://raw.githubusercontent.com/aprack/DGApp/refs/heads/main/dgdiscs.csv")
 
-st.title("CSV Query App")
+st.title("Disc Flight Number Search")
 column = st.selectbox("Choose a column to filter:", df.columns)
 value = st.number_input(f"Enter the value to filter in {column}:")
 
