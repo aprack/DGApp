4da050cd0dde217966fadf41786e1493c1d0b33f
aprack <andrew.prack@gmail.com>
2025-01-09 07:33:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ for col in ['Speed', 'Glide', 'Turn', 'Fade']:
     if col in df.columns:
         df[col] = pd.to_numeric(df[col], errors='coerce')
 
-st.title("Disc Flight Number and Text Search")
+st.title("Disc Search")
 
 # Let users select multiple columns to filter by
 selected_columns = st.multiselect("Choose columns to filter by:", available_columns)
